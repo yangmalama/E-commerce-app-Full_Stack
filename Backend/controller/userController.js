e3a307ba36793ab1ab2bacb003a6115c550815eb
Yangma Lama <yangma.lama02@gmail.com>
2025-03-04 12:34:38 +0545

    Auth updated

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,77 +1,139 @@
-import { User } from "../schema/userSchema.js"
+import { User } from "../schema/userSchema.js";
+import bcrypt from "bcrypt";
+const saltRounds = 10;
+import jwt from "jsonwebtoken";
 
 // create user
 //register
-export const registerUser = async (req, res)=> {}
+export const registerUser = async (req, res) => {
+  try {
+    // validation of email check if user already exist
+    const userExist = await User.findOne({ email: req.body.email });
+    if (userExist) {
+      return res.status(409).json({
+        message: "The user already exists or user name already taken",
+      });
+    }
+    // Hashing password before registeration
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+    console.log(hashedPassword);
+
+    const newRegisterUser = await new User({
+      ...req.body,
+      password: hashedPassword,
+    }).save();
+    return res.status(201).json({
+      message: "User registered sucessfully",
+      data: newRegisterUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error in creating/registering  user",
+      error: error,
+    });
+  }
+};
 
 // login
-export const loginUser = async (req, res)=> {}
+export const loginUser = async (req, res) => {
+  try {
+    // check if the user  exists or not
+    const userExist = await User.findOne({ email: req.body.email });
+    if (!userExist) {
+      return res.status(409).json({
+        message: "The user doesnot exists. Please register",
+      });
+    }
+    // if user exists compare the password
+    const isPasswordMatched = await bcrypt.compare(
+      req.body.password,
+      userExist.password
+    );
+    if (!isPasswordMatched) {
+      return res.status(401).json({
+        message: "Password not matched",
+      });
+    }
 
+    // if password matched generate the token
+    const myToken = jwt.sign({email: userExist.email}, "this is private_key", {
+      expiresIn: "24h",
+    });
+    console.log(myToken);
+    return res.status(201).json({
+      message: "User logged in sucessfully",
+      token: myToken,
+      user: userExist
+    })
 
+  } catch (error) {
+    return res.status(500).json({
+      message: " error in user login",
+      error: error,
+    });
+  }
+};
 
 //get all
-export const getAllUser = async (req, res)=> {
+export const getAllUser = async (req, res) => {
   try {
-    const allUser =  await User.find()
+    const allUser = await User.find();
     return res.status(200).json({
       message: "All users fetched sucessfully!!",
-      data: allUser
-    })
+      data: allUser,
+    });
   } catch (error) {
     return res.status(500).json({
       message: "Internal server error in fetching user",
-      error: error
+      error: error,
     });
   }
-}
+};
 
 //get user by id
-export const getUserById = async (req, res)=> {
+export const getUserById = async (req, res) => {
   try {
-    const singleUser = await User.findById()
+    const singleUser = await User.findById();
     return res.status(200).json({
       message: "Sinlge user fetched sucessfully!!",
-      data: singleUser
-    })
+      data: singleUser,
+    });
   } catch (error) {
     return res.status(500).json({
       message: "Internal server error in fetching user",
-      error: error
+      error: error,
     });
   }
-}
+};
 
 //update user by id
-export const updateUserById= async (req, res)=> {
+export const updateUserById = async (req, res) => {
   try {
-    const updateUser = await User.findByIdAndUpdate()
+    const updateUser = await User.findByIdAndUpdate();
     return res.status(200).json({
-      message:"User update sucessfull!!",
-      data: updateUser
-    })
+      message: "User update sucessfull!!",
+      data: updateUser,
+    });
   } catch (error) {
     return res.status(500).json({
       message: "Internal server error in updating user",
-      error: error
+      error: error,
     });
   }
-}
+};
 
 //delete user by id
-export const deleteUserById = async (req, res)=> {
+export const deleteUserById = async (req, res) => {
   try {
-    const deleteUser = await User.findByIdAndDelete()
+    const deleteUser = await User.findByIdAndDelete();
     return res.status(200).json({
       message: "User deleted sucessfully!!",
-      data: deleteUser
-    })
+      data: deleteUser,
+    });
   } catch (error) {
     return res.status(500).json({
       message: "Internal server error in deleting user",
-      error: error
+      error: error,
     });
   }
-}
-
-
-
+};
--- Backend/schema/userSchema.js
+++ Backend/schema/userSchema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // user schema
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  imageUrl: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 // user table
